753a1e01a404c3dd5fb96331b2965a0534a32d8c
Hector Carrasco <76969632+Hector-ACO@users.noreply.github.com>
2022-02-16 11:47:11 -0700

    Update User.java

--- genericos/src/main/java/com/demo/genericos/model/User.java
+++ genericos/src/main/java/com/demo/genericos/model/User.java
@@ -1,12 +1,8 @@
 package com.demo.genericos.model;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.UniqueElements;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
