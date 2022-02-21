aaea21fce78d51de6a8e1596b29e44fe4603a4f1
Hector Carrasco <76969632+Hector-ACO@users.noreply.github.com>
2022-02-21 08:16:43 -0700

    Delete genericos directory

--- genericos/src/main/java/com/demo/genericos/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.genericos.repository;
-
-import com.demo.genericos.model.User;
-
-public interface UserRepository extends GenericRepository<User>{
-    User findUserByUser(String user);
-}
